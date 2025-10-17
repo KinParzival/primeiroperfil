59b8e2b452d53688f22d2c35fd55e6f6fa32fccd
KinParzival <guilhermefranco934@gmail.com>
2025-10-17 15:10:33 -0300

    detalhes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function toggleMode() {
 
     if(html.classList.contains('light')) {
 
-    
+     img.setAttribute("src", "assets/perfil.png");
 
     } else {
         img.setAttribute("src", "./assets/perfil.png")
